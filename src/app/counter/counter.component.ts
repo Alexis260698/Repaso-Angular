1f90281a2cfcaa39be2b27569249d682b4769380
Alexis260698~ <hena260698@gmail.com>
2023-06-03 13:01:34 -0600

    repasando Angular

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -3,8 +3,24 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>',
-})
-export class CounterComponent{
+  template: `
 
+<h3>counter: {{counter}}</h3>
+<button (click)="increaseBy(+1)">+1</button>
+<button (click)="increaseBy(-1)">-1</button>
+<button (click)="reset()">Reset</button>
+
+  `,
+})
+export class CounterComponent {
+  public counter:number= 10;
+
+
+  increaseBy(value: number):void{
+  this.counter+=value;
+  }
+
+  reset(){
+    this.counter=10;
+  }
 }
